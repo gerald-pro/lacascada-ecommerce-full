be6e796e3292e7b826f8152e833981e0d945dc2e
Gerald Avalos <avaloss.gerald@gmail.com>
2024-07-20 19:45:21 -0400

    php artisan view:clear

--- storage/framework/views/cf8ff3a1a97392ca5254c1dee1d7bb00.php
+++
@@ -1,4 +0,0 @@
-<th <?php echo e($attributes->merge(['class' => 'bg-gray-100 dark:bg-gray-900 border border-light p-3'])); ?>>
-    <?php echo e($slot); ?>
-
-</th><?php /**PATH C:\laragon\www\lacascada-ecommerce\resources\views\components\th.blade.php ENDPATH**/ ?>
--- storage/framework/views/f010bba839c08fa4635bb7cfacf96aac.php
+++
@@ -1,3 +0,0 @@
-<?php
- echo cloudinary()->getImageTag($publicId ?? '')->scale($width ?? '', $height ?? '')->serialize();
-?><?php /**PATH C:\laragon\www\lacascada-ecommerce\vendor\cloudinary-labs\cloudinary-laravel\resources\views\components\image.blade.php ENDPATH**/ ?>
